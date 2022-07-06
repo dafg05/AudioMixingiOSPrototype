62ff413f6497a14fe722399fb8a443d7286ddb93
dafg05 <danialefloresg@gmail.com>
2022-07-06 16:31:48 -0500

    Add single track playing options

--- AudioMixing/AudioMixingViewController.m
+++ AudioMixing/AudioMixingViewController.m
@@ -15,7 +15,7 @@
 
 @property (strong,nonatomic) AVAudioEngine *audioEngine;
 @property (strong,nonatomic) AVAudioMixerNode *mixerNode;
-@property (strong,nonatomic) NSMutableArray *filePathArray;
+@property (strong,nonatomic) NSMutableArray *fileUrlArray;
 
 @end
 
@@ -24,30 +24,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Hard code file URLs
-    self.filePathArray = [[NSMutableArray alloc] init];
+    
     NSString *pathToUrl1 = [[NSBundle mainBundle] pathForResource:@"track1" ofType:@"mp3"];
+    NSURL *fileUrl1 = [NSURL fileURLWithPath:pathToUrl1];
     NSString *pathToUrl2 = [[NSBundle mainBundle] pathForResource:@"track2" ofType:@"mp3"];
+    NSURL *fileUrl2 = [NSURL fileURLWithPath:pathToUrl2];
     NSString *pathToUrl3 = [[NSBundle mainBundle] pathForResource:@"track3" ofType:@"mp3"];
-    [self.filePathArray addObject:pathToUrl1];
-    [self.filePathArray addObject:pathToUrl2];
-    [self.filePathArray addObject:pathToUrl3];
-}
-- (IBAction)didTapAVAudioEngine:(id)sender {
-    // code from: https://medium.com/@ian.mundy/audio-mixing-on-ios-4cd51dfaac9a
-    NSLog(@"file count: %lu", self.filePathArray.count);
+    NSURL *fileUrl3 = [NSURL fileURLWithPath:pathToUrl3];
+    
+    self.fileUrlArray = [[NSMutableArray alloc] init];
+    [self.fileUrlArray addObject:fileUrl1];
+    [self.fileUrlArray addObject:fileUrl2];
+    [self.fileUrlArray addObject:fileUrl3];
+    
     self.audioEngine = [[AVAudioEngine alloc] init];
     self.mixerNode = [[AVAudioMixerNode alloc] init];
-    
-    NSLog(@"On AVAudioEngine"); 
+
+}
+- (IBAction)didTapFullMix:(id)sender {
+    // code from: https://medium.com/@ian.mundy/audio-mixing-on-ios-4cd51dfaac9a
+    [self.audioEngine stop];
     [self.audioEngine attachNode:self.mixerNode];
     [self.audioEngine connect:self.mixerNode to:self.audioEngine.outputNode format:nil];
-    
+
     NSError * __autoreleasing *startError = NULL;
     BOOL results = [self.audioEngine startAndReturnError:startError];
+
     if (results){
-        for (NSString *filePath in self.filePathArray){
-            NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
-            
+        for (NSURL *fileUrl in self.fileUrlArray){
             AVAudioPlayerNode *playerNode = [[AVAudioPlayerNode alloc] init];
             [self.audioEngine attachNode:playerNode];
             
@@ -59,6 +63,40 @@
             [playerNode scheduleFile:file atTime:nil completionHandler:nil];
             [playerNode play];
         }
+    }
+    
+}
+- (IBAction)didTapTrack1:(id)sender {
+    [self playSingleTrack:self.fileUrlArray[0]];
+}
+
+- (IBAction)didTapTrack2:(id)sender {
+    [self playSingleTrack:self.fileUrlArray[1]];
+}
+
+- (IBAction)didTapTrack3:(id)sender {
+    [self playSingleTrack:self.fileUrlArray[2]];
+}
+
+
+
+-(void)playSingleTrack:(NSURL *) fileUrl{
+    [self.audioEngine stop];
+    NSError * __autoreleasing *readingError = NULL;
+    AVAudioFile *file = [[AVAudioFile alloc] initForReading:fileUrl.absoluteURL error:readingError];
+    
+    AVAudioPlayerNode *playerNode = [[AVAudioPlayerNode alloc] init];
+    [self.audioEngine attachNode:playerNode];
+    [self.audioEngine connect:playerNode to:self.audioEngine.outputNode format:file.processingFormat];
+    
+    NSError * __autoreleasing *startError = NULL;
+    BOOL results = [self.audioEngine startAndReturnError:startError];
+    [playerNode scheduleFile:file atTime:nil completionHandler:nil];
+    if (results){
+        [playerNode play];
+    }
+    else{
+        NSLog(@"An error occurred");
     }
 }
 
